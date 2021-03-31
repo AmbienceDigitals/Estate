6adf2b9eaaae2dd51b64c6e5ed3d871c451fa8dd
Ambience <ambiencemultimediakoncept@gmail.com>
2021-04-01 00:57:04 +0100

    form updates

--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TabsetComponent } from 'ngx-bootstrap/tabs';
-import { Iproperty } from '../iproperty.interface';
+import { IPropertyBase } from 'src/app/model/ipropertybase';
+
 
 
 @Component({
@@ -12,39 +13,139 @@ import { Iproperty } from '../iproperty.interface';
 })
 export class AddPropertyComponent implements OnInit {
 
-  @ViewChild('form') addPropertyForm: NgForm;
+  // @ViewChild('form') addPropertyForm: NgForm;
   @ViewChild('formTabs') formTabs: TabsetComponent;
   // SellRent = 1;
-  @Input() property: Iproperty;
+  @Input() property: IPropertyBase;
+
+  nextClicked: boolean;
+
+  addPropertyForm: FormGroup;
 
     // will come from masters
   propertyTypes: Array<string> = ['House', 'Apartment', 'Duplex'];
   furnishTypes: Array<string> = ['Fully', 'Semi', 'Unfurnished'];
 
-  propertyView: Iproperty = {
+  propertyView: IPropertyBase = {
     Id: null,
     Name: null,
     Price: null,
     SellRent: null,
-    Type: null
+    PType: null,
+    FType: null,
+    BHK: null,
+    BuiltArea: null,
+    City: null,
+    RTM: null
   };
 
-  constructor(private router: Router) { }
+  constructor(private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    this.createAddPropertyForm();
   }
 
-  onSubmit(Form: NgForm): NgForm {
-    console.log(Form.value);
-    Form.reset();
-    return Form.value;
+  createAddPropertyForm(): void {
+    this.addPropertyForm = this.fb.group({
+      // Grouping form controls
+      BasicInfo: this.fb.group({
+        SellRent: ['1', Validators.required],
+        PType: [null, Validators.required],
+        Name: [null, Validators.required],
+      }),
+
+      PriceInfo: this.fb.group({
+        Price: ['', Validators.required],
+        BuiltArea: [null, Validators.required],
+        CarpetArea: [null],
+        Security: [null],
+        Maintenance: [null]
+      }),
+
+      AddressInfo: this.fb.group({
+        FloorNo: [null],
+        TotalFloor: [null],
+        Adress: [null],
+        LandMark: [null]
+      }),
+
+      OtherInfo: this.fb.group({
+        RTM: [null],
+        PossessionOn: [null],
+        AOP: [null],
+        Gated: [null],
+        MainEntrance: [null],
+        Description: [null]
+      })
+      });
+  }
+
+
+//#region <GetterMethods>
+
+  //#region <FormGroups>
+
+      get BasicInfo(): FormGroup {
+        return this.addPropertyForm.controls.BasicInfo as FormGroup;
+      }
+
+      get PriceInfo(): FormGroup {
+        return this.addPropertyForm.controls.PriceInfo as FormGroup;
+      }
+
+      get AddressInfo(): FormGroup {
+        return this.addPropertyForm.controls.AddressInfo as FormGroup;
+      }
+
+      get OtherInfo(): FormGroup {
+        return this.addPropertyForm.controls.OtherInfo as FormGroup;
+      }
+
+  //#endregion
+
+  //#region <FormControls>
+    get SellRent(): FormControl {
+      return this.BasicInfo.controls.SellRent as FormControl;
+    }
+
+    get Price(): FormControl {
+      return this.PriceInfo.controls.Price as FormControl;
+    }
+
+    get RTM(): FormControl {
+      return this.OtherInfo.controls.RTM as FormControl;
+    }
+
+  //#endregion
+
+//#endregion
+
+  onSubmit(): void {
+    this.nextClicked = true;
+    if (this.BasicInfo.invalid) {
+      this.formTabs.tabs[0].active = true;
+      return;
+    }
+
+    if (this.PriceInfo.invalid) {
+      this.formTabs.tabs[1].active = true;
+      return;
+    }
+
+    console.log('Congrats, Form submitted');
+    console.log('SellRent = ' + this.addPropertyForm.value.BasicInfo.SellRent);
+    console.log(this.addPropertyForm);
+
   }
 
   // onBack() {
   //   this.router.navigate(['/']);
   // }
 
-  selectTab(tabId: number): void {
-    this.formTabs.tabs[tabId].active = true;
+  selectTab(NextTabId: number, IsCurrentTabValid: boolean): void {
+    this.nextClicked = true;
+    if (IsCurrentTabValid) {
+    this.formTabs.tabs[NextTabId].active = true;
+    }
   }
 }
--- Frontend/src/app/property/property-card/property-card.component.ts
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { faEdit, faAddressBook } from '@fortawesome/free-solid-svg-icons';
-import { Iproperty } from '../iproperty.interface';
+import { IPropertyBase } from 'src/app/model/ipropertybase';
 
 @Component({
   selector: 'app-property-card',
@@ -9,7 +9,11 @@ import { Iproperty } from '../iproperty.interface';
 })
 export class PropertyCardComponent implements OnInit {
 
-  @Input() property: Iproperty;
+  //  input to transfer the value of property from one component to another
+  @Input() property: IPropertyBase;
+
+  // input to hide icons conditionally
+  @Input() hideIcons: boolean;
 
   edit: any = faEdit;
   contact: any = faAddressBook;
